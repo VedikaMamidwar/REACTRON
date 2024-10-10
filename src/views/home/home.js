fc5f56e87a0a03d02a07d060e09f0e842fd17d75
VedikaMamidwar <vedikamamidwar5@gmail.com>
2024-10-10 22:23:05 +0530

    Updated home page and package files

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,15 +2,23 @@ import React from 'react';
 import Navbar from '../../Components/navbar/navbar';
 import homeImage from '../../views/home/Home screen-bro.png';
 import './home.css';
+import toast, { Toaster } from "react-hot-toast";
 
-function Home(){
+function Home() {
   return (
     <div>
       <Navbar />
       <h1>home</h1>
       <img src={homeImage} alt="Feedback" className="header-image" />
+
+      <button className="my-button" onClick={() => {
+        toast.loading("Loaded..");
+      }}>
+        Click me!!
+      </button>
+      <Toaster position:top-left />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
